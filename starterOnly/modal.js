6b8d310050873fed4491007550bed90c53bd0c20
MorganeLM <morganelemarquer@lilo.org>
2021-04-07 22:26:00 +0200

    close modal + select DOM elements

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,14 +10,35 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalClosingBtn = document.querySelector(".close")
 const formData = document.querySelectorAll(".formData");
+const form = document.querySelector('#inscriptionForm');
+
+const firstName = document.querySelector('#first');
+const lastName = document.querySelector('#last');
+const email = document.querySelector('#email');
+const birthDate = document.querySelector('#birthdate');
+const quantity = document.querySelector('#quantity');
+const towns = document.querySelectorAll(".location-checkbox");
+const useConditions = document.querySelector(".checkbox1")
+const nextEvent = document.querySelector(".checkbox2")
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// set modal close event
+modalClosingBtn.addEventListener("click", closeModal);
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+// check field validation before submit
+form.addEventListener("submit", function(){
+
+})
 
